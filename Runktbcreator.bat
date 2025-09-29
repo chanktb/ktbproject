82b40e53bff32406997dc298fcafbc3098d18b97
chanktb <khuedhsp@gmail.com>
2025-09-29 20:49:13 +0700

    Update via ktbimage tool - 2025-09-29 20:49:13

---
+++ Runktbcreator.bat
@@ -0,0 +1,20 @@
+@echo off
+REM =================================================================
+REM                      CAU HINH BAN DAU
+REM =================================================================
+REM Chuyen console sang che do UTF-8 de hien thi tieng Viet chinh xac.
+chcp 65001 >nul
+
+REM Buoc Python su dung UTF-8, giai quyet triet de loi UnicodeEncodeError.
+set PYTHONUTF8=1
+
+REM Dat tieu de cho cua so terminal.
+title ktbcreator
+
+REM Lấy thư mục hiện tại của file .bat
+cd /d "%~dp0"
+
+REM Chạy module Python
+python -m ktbcreator.main
+
+pause
